391e3e74fb377a72f2b073a055019fd289e2de26
Natalya <natalya.kim@kcl.ac.uk>
2017-05-27 16:02:07 +0100

    Add logic for Q1, Q3.

---
+++ logic/logic.js
@@ -0,0 +1,61 @@
+var docType
+var partyType
+var purpose
+var lenght
+var jurisdiction
+var startDate
+var otherPartyName
+var businessRegNum
+var businessAddress
+
+
+var answers = {
+  docType1 : ["Mutual", "mutual", "M", "m"],
+  docType2 : ["One Way", "One way", "one way", "One", "one", "O", "o"],
+  // ---------------------------------------- //
+  partyType1 : ["Receiving", "receiving", "R", "r"],
+  partyType2 : ["Sharing", "sharing", "S", "s"],
+  // ---------------------------------------- //
+  purpose1 : [],
+  purpose2 : [],
+  // ---------------------------------------- //
+  lenght1 : ["No", "no", "N", "n"],
+  length2 : ["Yes", "yes", "Y", "y"]
+};
+
+// Q1
+var getDocType = function (res) {
+  if (answers.docType1.includes(res)) {
+    docType = res
+    console.log(docType);
+  } else if (answers.docType2.includes(res)){
+    docType = res
+  } else {
+    console.log("You entered wrong answer.");
+  }
+};
+// Q1a
+var getPartyType = function (res) {
+  if (answers.partyType1.includes(res)) {
+    var partyType = res
+  } else if (answers.partyType2.includes(res)) {
+    var partyType = res
+  } else {
+    console.log("You entered wrong answer.");
+  }
+}
+// TODO Q2
+
+// Q3
+var getDocLength =  function(res) {
+  if (answers.lenght1.includes(res)) {
+    var lenght = "Indefinite"
+  } else if (answers.length2.includes(res)) {
+    var lenght = res
+  } else {
+    console.log("You entered wrong answer.");
+  }
+}
+
+// Q4
+getDocType("m");
